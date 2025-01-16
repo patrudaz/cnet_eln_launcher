018da408609997cc78f825a1e42dd18d047f621d
Patrice Rudaz <patrice.rudaz@hevs.ch>
2025-01-16 15:21:41 +0100

    Add remxttr.sh in .dmg package

--- remxttr.sh
+++ remxttr.sh
@@ -14,6 +14,13 @@
 # $1 = -R for recursive or -U for unrecursive. Must be specified
 # $2 = path to affect
 
+# Change the permissions
+sudo chflags -R nouchg CNetMinecraftLauncher.app
+sudo chmod -R u+r+w+x,g+r+w+x,o+r-w+x CNetMinecraftLauncher.app
+sudo chmod a+w CNetMinecraftLauncher.app/Contents/Resources/_internal/scripts/sh_log_cnet.txt
+sudo chmod -R -N CNetMinecraftLauncher.app
+
+
 oldIFS=$IFS # save the field separator  
 IFS=$'\n' # new field separator, the end of line  
 
@@ -22,7 +29,7 @@ then
 	echo "Recursive"
 	for i in $(ls -Rl@ $2 | grep '^	' | awk '{print $1}' | sort -u)
 	do echo Removing $i ...
-		find $2 -print0 | xargs -0t xattr -d $i 2>/dev/null ;
+		find $2 -print0 | xargs -0t sudo xattr -d $i 2>/dev/null ;
 	done
 
 fi
@@ -31,10 +38,10 @@ echo "Doing path itself"
 	
 #ls parent but grep exact path
 	
-for i in $(xattr $2 | awk 'BEGIN { FS = "\r" } ; {print $1}')
+for i in $(sudo xattr $2 | awk 'BEGIN { FS = "\r" } ; {print $1}')
 do echo Removing $i ...
 	#find $2 -print0 | xargs -0t xattr -d $i 2>/dev/null ;
-	xattr -d $i $2
+	sudo xattr -d $i $2
 done
 
 
